7b4ea2e1933b72cf4ca12ba63580d444dce7438a
Olga Lushnikova <63054910+basto13@users.noreply.github.com>
2023-01-13 11:51:21 +0200

    add telegram bot first function

---
+++ main.py
@@ -0,0 +1,14 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+app = ApplicationBuilder().token("5833371913:AAHZf7ENL87ZxYMRlz-4JZXq8NA368WlHbs").build()
+
+app.add_handler(CommandHandler("hello", hello))
+
+app.run_polling()
